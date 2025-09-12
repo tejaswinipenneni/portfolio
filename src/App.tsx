aca56ccbe83a7199025b4dadfa754d63243edc87
Tejaswini Penneni <tejaswinipenneni@gmail.com>
2025-09-12 11:24:39 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,18 @@ import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-white">
-//       <Header />
-//       <Hero />
-//       <About />
-//       <Experience />
-//       <Skills />
-//       <Contact />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-export default function App() {
-  return <h1>Hello 👋</h1>;
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <About />
+      <Experience />
+      <Skills />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
